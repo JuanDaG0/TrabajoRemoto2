b7af83670f8cab89eecc7a81730e396e09c89289
unknown <juandagoxx@gmail.com>
2026-02-05 17:25:14 -0500

    Plantilla

---
+++ main.py
@@ -0,0 +1,50 @@
+"""
+Calculadora de Salud - Equipo
+"""
+
+def calcular_imc(peso, altura):
+    """Calcula el indice de masa corporal"""
+    
+
+
+def calcular_grasa_corporal(imc, edad):
+    """Calcula el porcentaje de grasa corporal"""
+    
+
+
+def calcular_tmb(peso, altura, edad, sexo):
+    """Calcula la tasa metabolica basal"""
+    
+
+
+def calcular_calorias_adelgazar(tmb, actividad):
+    """Calcula calorias diarias para adelgazar"""
+    
+
+
+def menu():
+    print("===== CALCULADORA DE SALUD =====")
+    print("1. Calcular IMC")
+    print("2. Calcular Grasa Corporal")
+    print("3. Calcular TMB")
+    print("4. Calcular Calorias para Adelgazar")
+
+    opcion = input("Elige una opcion, solo el numero: ")
+
+    if opcion == "1":
+        print("IMC")
+
+    elif opcion == "2":
+        print("Grasa corporal")
+
+    elif opcion == "3":
+        print("TMB")
+
+    elif opcion == "4":
+        print("Calorias para adelgazar")
+
+    else:
+        print("No valido")
+
+
+menu()
