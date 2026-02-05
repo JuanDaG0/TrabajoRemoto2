6f8ee4e9ed90fe0cef275d76971d157e254ed22a
unknown <juandagoxx@gmail.com>
2026-02-05 17:51:18 -0500

    IMC

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ Calculadora de Salud - Equipo
 """
 
 def calcular_imc(peso, altura):
-    """Calcula el indice de masa corporal"""
+    imc = peso / (altura * altura) 
+    return imc
     
 
 
@@ -32,7 +33,21 @@ def menu():
     opcion = input("Elige una opcion, solo el numero: ")
 
     if opcion == "1":
-        print("IMC")
+        print("CALCULADORA DE IMC")
+        peso = float(input("Ingresa tu peso en kilogramos: "))
+        altura = float(input("Ingresa tu altura en metros: "))
+
+        imc = calcular_imc(peso, altura)
+        print("Tu IMC es:", round(imc, 2))
+
+        if imc < 18.5:
+            print("Clasificación: Bajo peso")
+        elif imc < 25:
+            print("Clasificación: Peso normal")
+        elif imc < 30:
+            print("Clasificación: Sobrepeso")
+        else:
+            print("Clasificación: Obesidad")
 
     elif opcion == "2":
         print("Grasa corporal")
